7145b294e58e5f8ea79cdbd11f981c126d006b58
vruvora <vvora8@gatech.edu>
2022-05-12 07:49:52 -0400

    add arxiv part

--- src/visualization/visualize_dashboard.py
+++ src/visualization/visualize_dashboard.py
@@ -4,6 +4,8 @@ import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
 import dash_table
+import random
+import networkx as nx
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -15,8 +17,8 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.H3('Keep Semi-Conductors Deflationary'),
-    dcc.Tabs(id="tabs-example-graph", value='tab-1-example-graph', children=[
-        dcc.Tab(label='Tab One', value='tab-1-example-graph'),
+    dcc.Tabs(id="tabs-example-graph", value='asym-science', children=[
+        dcc.Tab(label='Finding Asymetric Deep Tech Bets', value='asym-science'),
         dcc.Tab(label='Tab Two', value='tab-2-example-graph'),
         dcc.Tab(label='Talent & Energy Hubs', value='talent-map'),
 
@@ -27,18 +29,15 @@ app.layout = html.Div([
 @app.callback(Output('tabs-content-example-graph', 'children'),
               Input('tabs-example-graph', 'value'))
 def render_content(tab):
-    if tab == 'tab-1-example-graph':
+    if tab == 'asym-science':
         return html.Div([
-            html.H3('Tab content 1'),
-            dcc.Graph(
-                figure={
-                    'data': [{
-                        'x': [1, 2, 3],
-                        'y': [3, 1, 2],
-                        'type': 'bar'
-                    }]
-                }
-            )
+            html.H3('What are scientiests working on?'),
+            html.Div([
+                dcc.Graph(id='topic_ts', figure=build_topic_evolution())
+            ]),
+            html.Div([
+                dcc.Graph(id='topic_ts', figure=build_coauthorship_network())
+            ])
         ])
     elif tab == 'tab-2-example-graph':
         return html.Div([
@@ -57,6 +56,7 @@ def render_content(tab):
     elif tab == 'talent-map':
         df = pd.read_csv('data/processed/electricity_prices.csv').sort_values(by="Number of Indeed Jobs", ascending=False)
         return html.Div([
+            html.H3("Where can we build Semi Infrastructure?"),
             html.Div([
               dcc.Graph(id='map', figure=build_map())
             ]),
@@ -69,6 +69,97 @@ def render_content(tab):
                 data=df.to_dict('records'),
             )
         ])
+
+
+def build_coauthorship_network():
+    edge_x = []
+    edge_y = []
+    filtered_authors = pd.read_csv('data/processed/filtered_authors.csv')
+    topic_per_author = pd.read_csv('data/processed/topic_per_author.csv').set_index('author1')
+
+    G = nx.from_pandas_edgelist(filtered_authors, source='author1', target='author2')
+    node_colors = pd.read_csv('data/processed/node_colors.csv').set_index('author1')
+    color_map_plotly = []
+    colors_plotly = []
+    for _ in range(14):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        rgb = f"rgb({r},{g},{b})"
+        colors_plotly.append(rgb)
+    for node in G:
+        try:
+            color_map_plotly.append(colors_plotly[node_colors.loc[node].values[0]])
+        except:
+            color_map_plotly.append(colors_plotly[13])
+
+    G_spring = nx.spring_layout(G)
+    for edge in G.edges():
+        x0, y0 = G_spring[edge[0]]
+        x1, y1 = G_spring[edge[1]]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = G_spring[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=False,
+            size=10,
+            line_width=2)
+    )
+    node_adjacencies = []
+    node_text = []
+    for node, adjacencies in enumerate(G.adjacency()):
+        node_adjacencies.append(len(adjacencies[1]))
+        try:
+            author = list(G.nodes())[node]
+            node_text.append(f'Author: {author}, Topic: {topic_per_author.loc[author].values[0]}, # of connections: '+str(len(adjacencies[1])))
+        except KeyError:
+            author = list(G.nodes())[node]
+            node_text.append(f'Author: {author}, # of connections: '+str(len(adjacencies[1])))
+
+    node_trace.marker.color = color_map_plotly
+    node_trace.text = node_text
+    fig = go.Figure(data=[edge_trace, node_trace],
+                layout=go.Layout(
+                    title='Co-Authorship Networks',
+                    titlefont_size=16,
+                    showlegend=False,
+                    hovermode='closest',
+                    margin=dict(b=20,l=5,r=5,t=40),
+                    annotations=[ dict(
+                        text='',
+                        showarrow=False,
+                        xref="paper", yref="paper",
+                        x=0.005, y=-0.002 ) ],
+                    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
+    return fig
+
+def build_topic_evolution():
+    topic_evol_df = pd.read_csv('data/processed/topic_labels.csv')
+    fig = px.line(topic_evol_df, x="date", y=topic_evol_df.columns, hover_data={"date": "|%B %d, %Y"}, title='HPC Topic Evolution: 2010-2021 (~10K Papers)')
+    return fig
 
 def build_map():
     indeed_lat_long = pd.read_csv('data/processed/indeed_lat_long.csv')
